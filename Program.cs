74f6d9f5c04fd3c7d88fad7eaed35c66fec9f41b
DubrovRuslan <dubrov.ruslan@gmail.com>
2023-08-01 13:13:24 +0300

    Done

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Module4HW4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("config.json");
+            var config = builder.Build();
+            var connectionString = config.GetConnectionString("DefaultConnection");
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationContext>();
+            var options = optionsBuilder.UseSqlServer(connectionString).Options;
+            using (var db = new ApplicationContext(options))
+            {
+                db.SaveChanges();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
